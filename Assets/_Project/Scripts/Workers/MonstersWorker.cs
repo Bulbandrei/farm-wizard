e05cdb9333cbb9c72ee1df5aa0fa327feff71d67
CASA-KURO\Elias <eliassanto@hotmail.com>
2021-05-08 18:55:36 -0300

    Monsters
    MonstersWorker
    MonsterPrefab

---
+++ Assets/_Project/Scripts/Workers/MonstersWorker.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MonstersWorker : MonoBehaviour
+{
+    public Sprite[] monstersSprites;
+
+    public Monster monsterPrefab;
+
+    public void SpawnRandomMonster(Vector2 p_position)
+    {
+
+    }
+}
